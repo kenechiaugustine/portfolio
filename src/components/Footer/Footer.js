74aea203cbb7fef7f348c2dc0e063048ccb3c6a1
Kene Arionye <kenechiaugustine@gmail.com>
2022-07-22 07:36:15 +0100

    footer link redirect to home

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/kenechiaugustine'
+      href='/'
       className='link footer__link'
     >
       Created By Kene Arionye
