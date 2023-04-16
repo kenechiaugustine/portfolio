0ca3a39d180b5fd1ce80d425a56ae2008a7c6876
Kenechukwu Arionye <kenechiaugustine@gmail.com>
2023-04-16 22:02:39 +0100

    changed some words

--- src/portfolio.js
+++ src/portfolio.js
@@ -6,8 +6,10 @@ const header = {
 const about = {
   name: 'Kenechukwu Arionye',
   role: 'Software Engineer',
-  description: "I am a software engineer with a passion for building software that is user-friendly and easy to use. I'm valued for driving quality and consistency throughout all phases of software development. Collaborating effectively with Backend Engineers, Frontend Engineers and UI/UX Engineers.",
-  resume: 'https://docs.google.com/document/d/1xQJC2SRCvshd8p55IJlGdElhZDLRwJfu00GeuooQBuw/edit?usp=sharing',
+  description:
+    "I am a software engineer with a passion for building software that is user-friendly and easy to use. I'm valued for driving quality and consistency throughout all phases of software development. Collaborating effectively with Backend Engineers, Frontend Engineers and UI/UX Engineers.",
+  resume:
+    'https://docs.google.com/document/d/1xQJC2SRCvshd8p55IJlGdElhZDLRwJfu00GeuooQBuw/edit?usp=sharing',
   social: {
     linkedin: 'https://www.linkedin.com/in/kenechukwu-arionye-57759722a/',
     github: 'https://github.com/kenechiaugustine',
@@ -16,17 +18,26 @@ const about = {
 
 const projects = [
   {
-    name: "PEIWA",
+    name: 'PEIWA',
     description:
-      'An Estate Management App, which will enable admin to manage PEIWA estate. I collaborated with Backend engineers to deliver this app.',
-    stack: ['PHP', 'Laravel', 'MySQL', 'HTML', 'CSS', 'Bootstrap', 'Javascript', 'jQuery'],
+      'I collaborated with Backend Engineers to deliver an Estate Management App that enables administrators to manage PEIWA estate.',
+    stack: [
+      'PHP',
+      'Laravel',
+      'MySQL',
+      'HTML',
+      'CSS',
+      'Bootstrap',
+      'Javascript',
+      'jQuery',
+    ],
     sourceCode: '#',
     livePreview: 'http://peiwasites.com/',
   },
   {
     name: 'Laragigs',
     description:
-      'Laragigs is a job listing app that allows users to share job openings. It is a web app that is built with Laravel  and MySQL. It is a fully functional app that allows authenticated users to create, read, update and delete job listings.',
+      'Laragigs is a web app built with Laravel and MySQL, designed for job listings where authenticated users can create, read, update and delete job openings. This fully functional app enables users to share job listings with ease.',
     stack: ['PHP', 'Laravel', 'SQL', 'Tailwind'],
     sourceCode: 'https://github.com/kenechiaugustine/laravel-listings',
     livePreview: 'https://laragigs.onrender.com/',
@@ -34,7 +45,7 @@ const projects = [
   {
     name: 'WhisperApp',
     description:
-      'Whisper is a social and dating app that allows users find friends, build relationships and chat with each other. It is built with NodeJS, Express, and React. The mobile app was built with Dart and Flutter. Currently, it is still in development.',
+      'Whisper is a social and dating app that is currently in development. The app is designed to enable users to find friends, build relationships, and chat with each other. It is built with NodeJS, Express, and React for the web version, and with Dart and Flutter for the mobile version.',
     stack: ['NodeJS', 'ExpressJS', 'TypeScript', 'React', 'Dart', 'Flutter'],
     sourceCode: '#',
     livePreview: 'https://whisperapp.co.uk/',
@@ -42,7 +53,7 @@ const projects = [
   {
     name: "Okonor's Crib",
     description:
-      'An assessment app, which will enable students to read and practice exams by taking quizzes. I collaborated with UX designers, Frontend engineers and Backend engineers to deliver this app.',
+      'This app is an assessment app that enables students to read and practice exams through taking quizzes. It involves the collaboration of UX designers, Frontend Engineers, and Backend Engineers in its development.',
     stack: ['NestJS', 'TypeScript', 'Postgres', 'React'],
     sourceCode: '#',
     livePreview: 'https://okonorlistic.com/',
@@ -51,11 +62,11 @@ const projects = [
   {
     name: 'Tuzz',
     description:
-      'A Fun and Awarding Application, where a user can choose a wild variety of games like Ludo, Whoot, Ayo and even Fantasy Premier League (FPL)',
+      'This application offers users a fun and rewarding experience by providing a wide variety of games to choose from, including Ludo, Whoot, Ayo, and even Fantasy Premier League (FPL).',
     stack: ['Flutter', 'Dart', 'NodeJS', 'ExpressJS', 'TypeScript'],
     sourceCode: '#',
     livePreview: 'https://tuzzapp.com/',
-  }
+  },
 ]
 
 const skills = [
