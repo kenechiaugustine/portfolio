1d713155dc40031c5f82b572e656fd2036b51494
Kenechukwu Arionye <kenechiaugustine@gmail.com>
2023-04-25 16:10:06 +0100

    more info for email

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,7 +7,9 @@ const Contact = () => {
   return (
     <section className='section contact center' id='contact'>
       <h2 className='section__title'>Contact</h2>
-      <a href={`mailto:${contact.email}`}>
+      <a
+        href={`mailto:${contact.email}?subject='Request for collaboration'&body='Hello Kene, Your resume is very enticing, and I would like to propose a collaboration with you on a project.'`}
+      >
         <span type='button' className='btn btn--outline'>
           Email me
         </span>
